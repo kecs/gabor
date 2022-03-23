5fa9c70040c8a7cf0490ace616e5760c7e14b63c
Mihaly Gabor <mihaly.gabor.dev@gmail.com>
2022-03-23 10:46:59 +0100

    URL tidy + landing page link

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from .views import (ArticleDetailView, ArticleListView)
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
-    path('/articles/<str:tag>/', ArticleListView.as_view(), name='article_list_by_tag'),
+    path('articles/<str:tag>/', ArticleListView.as_view(), name='article_list_by_tag'),
 ]
